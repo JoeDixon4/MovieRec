681b14e433ab80b0f716e66ce17e611809ffe61a
Joe Dixon <joedixon4@outlook.com>
2022-10-31 12:37:41 +0000

    Coverage Update

--- src/test/java/ApiComponents/demo/ActorTest.java
+++ src/test/java/ApiComponents/demo/ActorTest.java
@@ -1,13 +1,18 @@
 package ApiComponents.demo;
-
+import ApiComponents.demo.Actor;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class ActorTest{
+    Actor myTestActor = new Actor("Joseph", "Dixon");
     @Test
-    public void testActorName(){
-        Actor myTestActor = new Actor("Joseph", "Dixon");
+    public void testFirstActorName(){
         String actualName  = myTestActor.getFirstName();
         Assertions.assertEquals("Joseph", actualName,"First names assigning incorrectly.");
+    }
+    @Test
+    public void testLastActorName(){
+        String actualName  = myTestActor.getLastName();
+        Assertions.assertEquals("Dixon", actualName,"Last names assigning incorrectly.");
     }
 }
