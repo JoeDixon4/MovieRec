dabe4755b3e1cccbb39ca1349ab8007e1ae12bc6
Joe Dixon <joedixon4@outlook.com>
2022-10-31 14:23:18 +0000

    Further Coverage

--- src/test/java/ApiComponents/demo/ActorTest.java
+++ src/test/java/ApiComponents/demo/ActorTest.java
@@ -6,13 +6,25 @@ import org.junit.jupiter.api.Assertions;
 public class ActorTest{
     Actor myTestActor = new Actor("Joseph", "Dixon");
     @Test
-    public void testFirstActorName(){
+    public void testGetFirstActorName(){
         String actualName  = myTestActor.getFirstName();
-        Assertions.assertEquals("Joseph", actualName,"First names assigning incorrectly.");
+        Assertions.assertEquals("Joseph", actualName,"First names are getting incorrectly.");
     }
     @Test
-    public void testLastActorName(){
+    public void testGetLastActorName(){
         String actualName  = myTestActor.getLastName();
-        Assertions.assertEquals("Dixon", actualName,"Last names assigning incorrectly.");
+        Assertions.assertEquals("Dixon", actualName,"Last names are getting incorrectly.");
+    }
+    @Test
+    public void testSetFirstActorName(){
+        myTestActor.setFirstName("Ryan");
+        String actualName = myTestActor.getFirstName();
+        Assertions.assertEquals("Ryan", actualName, "First names are setting incorrectly");
+    }
+    @Test
+    public void testSetLastActorName(){
+        myTestActor.setLastName("Garrett");
+        String actualName = myTestActor.getLastName();
+        Assertions.assertEquals("Garrett", actualName, "Last names are setting incorrectly");
     }
 }
