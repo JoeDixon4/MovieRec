fbdf04354df7d86f8c98044be995cde0de400ed5
Joe Dixon <joedixon4@outlook.com>
2022-10-31 11:09:51 +0000

    Updated

---
+++ src/test/java/ApiComponents/demo/ActorTest.java
@@ -0,0 +1,12 @@
+package ApiComponents.demo;
+
+import org.junit.jupiter.api.Assertions;
+
+public class ActorTest{
+
+    public void ActorNameTest(){
+        Actor myTestActor = new Actor("Joseph", "Dixon");
+
+       // Assertions.assertEquals("Joseph",Actor.getFirstName(myTestActor),"First names assigning incorrectly.");
+    }
+}
