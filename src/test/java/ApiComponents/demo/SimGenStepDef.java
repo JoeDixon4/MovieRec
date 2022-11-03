d6cfc4c779014da547bcd6f96861cc042a03c8cd
Joe Dixon <joedixon4@outlook.com>
2022-11-03 17:03:19 +0000

    Cucumber Working

--- src/test/java/ApiComponents/demo/SimGenStepDef.java
+++ src/test/java/ApiComponents/demo/SimGenStepDef.java
@@ -3,24 +3,38 @@ package ApiComponents.demo;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.spring.ScenarioScope;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+@ScenarioScope
 public class SimGenStepDef {
-    @Given("a users favourite movie genre")
-    public void a_users_favourite_movie_genre() {
+    private String genre;
+
+    @Autowired
+    FilmRepo filmRepo;
+    @Autowired
+    CountryRepo countryRepo;
+    @Autowired
+    ActorRepo actorRepo;
+    @Autowired
+    MovieRecApplication movieRecApplication = new MovieRecApplication(actorRepo, filmRepo, countryRepo);
+    private List<Film> films;
+    @Given("a user has a favourite movie genre")
+    public void a_user_has_a_favourite_movie_genre() {
         // Write code here that turns the phrase above into concrete actions
-        //int chosenGenre = "Animation";
-        //throw new io.cucumber.java.PendingException();
+        genre = "Drama";
     }
-    @When("the user searches for the genre")
-    public void the_user_searches_for_the_genre() {
+    @When("the program takes the users movie genre")
+    public void the_program_takes_the_users_movie_genre() {
         // Write code here that turns the phrase above into concrete actions
-        //get mapping all movies with genre chosenGenre
-        throw new io.cucumber.java.PendingException();
+        List<Film> films = movieRecApplication.getFilmByCategory(genre);
     }
     @Then("the program returns popular movies of the same genre")
     public void the_program_returns_popular_movies_of_the_same_genre() {
         // Write code here that turns the phrase above into concrete actions
-        //return all movies with the Genre
-        throw new io.cucumber.java.PendingException();
+        System.out.println(films);
     }
 }
--- src/test/resources/Cucumber/SimilarGenre.feature
+++ src/test/resources/Cucumber/SimilarGenre.feature
@@ -1,8 +1,8 @@
-Feature: This feature will search for similar genres of a chosen movie.
+Feature: This feature will search for movies with the same genre.
   As a user, I would like to be able to choose a genre so that I can have movies of the genre recommended.
 
   Scenario: Successfully retrieving movies of the chosen genre.
-    Given  a users favourite movie genre
-    When  the user searches for the genre
+    Given  a user has a favourite movie genre
+    When  the program takes the users movie genre
     Then  the program returns popular movies of the same genre
 
